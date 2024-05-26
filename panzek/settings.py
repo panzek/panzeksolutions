9abdf71eafde920101396a33fd798c5acb54c1f5
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-05-26 15:33:46 +0100

    Add INTERNAL_IPS

--- panzek/settings.py
+++ panzek/settings.py
@@ -50,6 +50,10 @@ INSTALLED_APPS = [
 
 TAILWIND_APP_NAME = 'theme'
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
