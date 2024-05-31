1da4ded47e0f04c68d904818dae7123160c57f90
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-05-31 19:18:41 +0100

    Update package.json to setup css file at project root level

--- panzek/settings.py
+++ panzek/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # needs to be a tuple - note the extra comma
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
