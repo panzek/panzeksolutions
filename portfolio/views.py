7888a384eb21d95222b40a45f600e76f52fec1ac
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-06-03 17:25:59 +0100

    Configure portfolio views and add portfolio template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """ A view to render the home page """
+
+    return render(request, 'portfolio/portfolios.html')
