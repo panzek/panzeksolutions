6383389b237f876095cf6545c3300db980e61bba
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-06-04 17:02:27 +0100

    Add portfolio app, update views.py and urls.py with code

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def portfolio(request):
+def portfolios(request):
     """ A view to render the portfolio page """
 
     template = 'portfolio/portfolios.html'
