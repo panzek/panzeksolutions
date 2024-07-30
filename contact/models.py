429bbf80ef591a6382b75139e354bfc4a8bb782a
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-07-30 21:19:10 +0100

    Add code to contact model and create forms.py

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Contact model for site visitors to contact us
+    """
+
+    firstName = models.CharField(max_length=100, null=False, blank=False)
+    lastName = models.CharField(max_length=200, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    subject = models.CharField(max_length=200, null=False, blank=False)
+    message = models.TextField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"  # using f-strings
