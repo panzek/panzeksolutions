f26edb8d4e1dc66c1d6712836fc0ce3d3210ff90
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-07-30 21:55:39 +0100

    Add code to forms.py

---
+++ contact/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from contact.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    """
+    A form for visitors to send us a message
+    """
+
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}))
+
+    class Meta:
+        model = Contact
+        fields = (
+            'firstName',
+            'lastName',
+            'email',
+            'subject',
+            'message',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'firstName': 'Enter your first name',
+            'lastName': 'Enter your last name',
+            'email': 'Enter your email address',
+            'subject': 'Enter your message subject',
+            'message': 'Enter your message',
+        }
+
+        self.fields['firstName'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'form-control'
