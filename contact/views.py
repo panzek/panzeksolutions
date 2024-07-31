adb3f1bb4b62344098c528627b69a54275f640d9
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-07-31 14:47:52 +0100

    Add code to view to render form in template

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.mail import send_mail
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    """
+    A view to render form in template
+    """
+
+    ContactForm(request.POST)
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            customer_message = form.save()
+            contact_email = customer_message.email
+            contact_subject = customer_message.subject
+            contact_message = customer_message.message
+
+            send_mail(
+                contact_subject,  # subject
+                contact_message,  # message
+                contact_email,  # from email
+                [settings.DEFAULT_FROM_EMAIL],  # to email
+            )
+
+    else:
+        form = ContactForm()
+
+        template = 'contact/contact.html'
+        context = {
+            'form': form,
+        }
+
+        return render(request, template, context)
