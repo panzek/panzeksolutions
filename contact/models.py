58f98d3461b8f3e0489d1bf7ff2d8018dbba789b
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-08-02 13:56:02 +0100

    Redefine select subject options

--- contact/models.py
+++ contact/models.py
@@ -6,17 +6,19 @@ class Contact(models.Model):
       Contact model for users to contact the company
     """
     SUBJECT_CHOICES = [
-        (1, 'API Integration'),
-        (2, 'CMS Integration'),
-        (3, 'eCommerce Web Design'),
-        (4, 'Site Optimization(SEO)'),
-        (5, 'Web Development'),
-        (6, 'Web Hosting'),
+        ('', 'Please select one'),
+        ('API', 'API Integration'),
+        ('CMS', 'CMS Integration'),
+        ('eComm', 'eCommerce Web Design'),
+        ('SEO', 'Site Optimization (SEO)'),
+        ('wDev', 'Web Development'),
+        ('wHost', 'Web Hosting'),
+        ('Other', 'Other'),
     ]
     firstName = models.CharField(max_length=100, null=False, blank=False)
     lastName = models.CharField(max_length=200, null=False, blank=False)
     email = models.EmailField(max_length=100, null=False, blank=False)
-    subject = models.CharField(max_length=100, choices=SUBJECT_CHOICES, null=False, blank=False)
+    subject = models.CharField(max_length=5, choices=SUBJECT_CHOICES, null=False, blank=False)
     message = models.TextField(max_length=500, null=False, blank=False)
 
     def __str__(self):
