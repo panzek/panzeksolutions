b6c519177cfa41f036bd31f50fa2471c22d7baf2
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-08-05 18:14:49 +0100

    Update npm path configuration

--- panzek/settings.py
+++ panzek/settings.py
@@ -83,8 +83,8 @@ INTERNAL_IPS = [
 if 'DEPLOYMENT' in os.environ:
     NPM_BIN_PATH = r"C:/Program Files/nodejs/npm.cmd"
 else:
-    NPM_BIN_PATH = os.environ.get('NPM_BIN_PATH', '~/.nvm/versions/node/v20.12.2/bin/npm')
-    NODE_BIN_PATH = os.environ.get('NODE_BIN_PATH', '~/.nvm/versions/node/v20.12.2/bin/node')
+    NPM_BIN_PATH = "$PATH:/.nvm/versions/node/v20.16.0/bin/npm"
+    NODE_BIN_PATH = "$PATH:/.nvm/versions/node/v20.16.0/bin/node"
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
