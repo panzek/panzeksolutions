a437c4ab9f48a1a9b5892f95d7e221025f23050b
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-08-12 11:49:32 +0100

    Add code to views.py and create urls path to map the view

--- solutions/views.py
+++ solutions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from solutions.models import Solution
+from django.views import generic
 
-# Create your views here.
+
+# using class-based view
+class SolutionListView(generic.ListView):
+    model = Solution
+    queryset = Solution.objects.all()
+    template_name = 'solution_list.html'
+    paginate_by = 4
+
