278800301de143ec95bb9b01243cf3d1955ade30
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-10-10 15:01:05 +0100

    Install sitemap app and migrate

--- panzek/settings.py
+++ panzek/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 
     'allauth',
     'allauth.account',
