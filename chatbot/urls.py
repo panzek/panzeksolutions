2e98fa63888768e6af13b2bec12c6f5091dc2b10
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2025-08-21 16:34:17 +0100

    Update urls for upload_document views

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from . import views, views_upload
 
 urlpatterns = [
     path('api/chat/', views.chat_api, name="chat_api"), # POST API endpoint for frontend fetch
+    path('api/upload/', views_upload.upload_document, name="upload_document"), # POST API endpoint for file upload
 ]
 
+             
