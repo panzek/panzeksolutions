070f0b49312880912c48422b9509beee06956a58
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2025-08-21 16:36:32 +0100

    Fix upload_document views path

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,5 +5,3 @@ urlpatterns = [
     path('api/chat/', views.chat_api, name="chat_api"), # POST API endpoint for frontend fetch
     path('api/upload/', views_upload.upload_document, name="upload_document"), # POST API endpoint for file upload
 ]
-
-             
